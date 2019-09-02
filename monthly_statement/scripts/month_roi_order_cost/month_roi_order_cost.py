85cd818b94a31e4ad26d44d16b376f51b8bac62b
liuhang <liuhang@aplum.com.cn>
2019-09-02 13:30:01 +0800

    xiugai

--- monthly_statement/scripts/month_roi_order_cost/month_roi_order_cost.py
+++ monthly_statement/scripts/month_roi_order_cost/month_roi_order_cost.py
@@ -402,28 +402,26 @@ def get_source_by_by_sql(db_market, sql):
 if __name__ == '__main__':
     result_dict = dict()
     # start_date, end_date = get_start_end_date()
-    # date_list = ['2016-10-01', '2016-11-01', '2016-12-01', '2017-01-01', '2017-02-01', '2017-03-01', '2017-04-01',
-    #              '2017-05-01', '2017-06-01', '2017-07-01', '2017-08-01', '2017-09-01', '2017-10-01', '2017-11-01',
-    #              '2017-12-01', '2018-01-01', '2018-02-01', '2018-03-01', '2018-04-01',
+    # date_list = ['2017-12-01', '2018-01-01', '2018-02-01', '2018-03-01', '2018-04-01',
     #              '2018-05-01', '2018-06-01', '2018-07-01', '2018-08-01', '2018-09-01', '2018-10-01', '2018-11-01',
     #              '2018-12-01', '2019-01-01', '2019-02-01', '2019-03-01', '2019-04-01',
     #              '2019-05-01', '2019-06-01', '2019-07-01']
-    date_list = ['2019-08-01']
+    date_list = ['2017-12-01']
     # print(date_list)
     source_dict = dict()
     db_market = MySQLdb.connect(mysql_host, mysql_user, mysql_passwd, mysql_db, charset='utf8')
     db_aplum = MySQLdb.connect(mysqlhost, mysqlusername, mysqlpasswd, db, charset='utf8')
     source_dict = get_source(db_market, source_dict)
-    # source_list = ['all', 'nature', 'channel_all', 'channel_flow', 'channel_kol']
-    source_list = ['channel_kol', 'channel_all']
+    source_list = ['all', 'nature', 'channel_all', 'channel_flow', 'channel_kol']
+    # source_list = ['channel_kol', 'channel_all']
     # # source_list = ['all', 'nature']
-    # for k in source_dict.keys():
-    #     # 来源
-    #     source = str(source_dict[k]['second_name'])
-    #     if source in source_list:
-    #         continue
-    #     else:
-    #         source_list.append(source)
+    for k in source_dict.keys():
+        # 来源
+        source = str(source_dict[k]['second_name'])
+        if source in source_list:
+            continue
+        else:
+            source_list.append(source)
     print("source_list:" + str(len(source_list)))
     for source in source_list:
         for current_date in date_list:
